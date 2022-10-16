20056cdafb21e65899363bfdf59b8e721af3e18d
suni21 <83060711+suni21@users.noreply.github.com>
2022-10-16 13:20:41 +0530

    new

--- Basics/InvertedStarPattern.c
+++ Basics/InvertedStarPattern.c
@@ -8,10 +8,11 @@ int main()
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
     {
-        for(int j=0;j<=n-i;j++)
+        for(int j=1;j<=i-n;j++)
         {
             printf(" ");
         }
+        
         printf("\n");
     }
     return 0;
